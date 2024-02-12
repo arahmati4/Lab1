2df23e081a2950909da260a5b46034b9d6fe512c
arahmati4 <arahmati4@myseneca.ca>
2024-02-12 19:00:07 +0000

    Removed lab2_funactions.py file

--- lab2_functions.py
+++
@@ -1,8 +0,0 @@
-def calculate_age():
-    birth_year = 1993
-    current_year = 2024
-    age = current_year - birth_year
-    print("Your age is:", 30
-
-# Call the function to test it
-calculate_age()
--- lab3_functions.py
+++
@@ -1,6 +0,0 @@
-def write_text_file():
-    with open("my_name.txt", "w") as file:
-        file.write Ashkan
-
-# Call the function to test it
-write_text_file()
