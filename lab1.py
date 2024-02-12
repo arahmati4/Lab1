e618543ac717fbe6bac0d07645225d99bbe8dc52
arahmati4 <arahmati4@myseneca.ca>
2024-02-12 18:37:40 +0000

    Added lab1.py with function to calculate age

---
+++ lab1.py
@@ -0,0 +1,8 @@
+def calculate_age():
+    birth_year = 1993 
+    current_year = 2024
+    age = current_year - birth_year
+    print("Your age is:", age)
+
+# Call the function to test it
+calculate_age()
