039e308dd5bb89a2df989c65ab3c11c27086e325
arahmati4 <arahmati4@myseneca.ca>
2024-02-12 20:36:23 +0000

    Remove Lab1, Lab2, and Lab3 directories

--- lab1_functions.py
+++
@@ -1,9 +0,0 @@
-def calculate_age():
-    birth_year = 1993  # Replace with your actual birth year
-    current_year = 2024
-    age = current_year - birth_year
-    print("Your age is:", age)
-
-# Call the function to test it
-calculate_age()
-
